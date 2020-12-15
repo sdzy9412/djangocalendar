6cbf3574d7c05fee19bfe51313cc8889324cea32
YiZhang <56272051+sdzy9412@users.noreply.github.com>
2020-12-15 20:17:50 +0000

    first commit

---
+++ calendar_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from calendar_app.models import Event, EventMember
+
+class EventMemberAdmin(admin.ModelAdmin):
+    model = EventMember
+    list_display = ['event', 'user']
+
+admin.site.register(Event)
+admin.site.register(EventMember, EventMemberAdmin)
