041fa28d2602b8c86de2f2b38e694511cd87d995
YiZhang <56272051+sdzy9412@users.noreply.github.com>
2020-12-27 00:27:49 +0000

    event id list: not only the first eventid found by userid to show in the calendar

--- calendar_app/utils.py
+++ calendar_app/utils.py
@@ -23,13 +23,21 @@ class Calendar(HTMLCalendar):
 
 	# formats a day as a td
 	# filter events by day
-	def formatday(self, day, events, eventmember):
-		if eventmember.exists():
-			eventid = eventmember[0].event_id
+	def formatday(self, day, events, eventmembers):
+		eventidlist = []
+		if eventmembers.exists():
+			for eventmember in eventmembers:
+				eventid = eventmember.event_id
+				eventidlist.append(eventid)
 		else:
-			eventid = 9999
+			eventidlist = [9999]
+		# print("event id for current user:",eventid)
+		# print("test - eventmember[1].event_id: ",eventmembers[1].event_id)
 		userid=get_current_userid()
-		events_per_day = events.filter(Q(Q(start_time__day=day)) & Q(Q(id = eventid)| Q(user_id =userid)))
+		print("event id list: ",eventidlist)
+		events_per_day = set()
+		for eventid in eventidlist:
+			events_per_day.update(events.filter(Q(Q(start_time__day=day)) & Q(Q(id = eventid)| Q(user_id =userid))))
 		d = "<div>"
 		
 		for event in events_per_day:
@@ -40,10 +48,10 @@ class Calendar(HTMLCalendar):
 		return '<td></td>'
 
 	# formats a week as a tr 
-	def formatweek(self, theweek, events,eventmember):
+	def formatweek(self, theweek, events,eventmembers):
 		week = ''
 		for d, weekday in theweek:
-			week += self.formatday(d, events,eventmember)
+			week += self.formatday(d, events,eventmembers)
 		return f"<tr> {week} </tr>"
 
 	# formats a month as a table
@@ -51,10 +59,10 @@ class Calendar(HTMLCalendar):
 	def formatmonth(self, withyear=True):
 		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
 		userid=get_current_userid()
-		eventmember = EventMember.objects.filter(user_id=userid)
+		eventmembers = EventMember.objects.filter(user_id=userid)
 		cal =f'<table border="0" cellpadding="0" cellspacing="0" class=" calendar calendar-table table table-condensed table-tight">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
 		cal += f'{self.formatweekheader()}\n'
 		for week in self.monthdays2calendar(self.year, self.month):
-			cal += f'{self.formatweek(week, events,eventmember)}\n'
+			cal += f'{self.formatweek(week, events,eventmembers)}\n'
 		return cal
